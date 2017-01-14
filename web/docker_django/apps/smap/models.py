31e794a9f43313db7877e379eda63a21dbcc817b
uehara1414 <b1015062@fun.ac.jp>
2017-01-14 17:13:12 +0900

    Add models and tests

--- web/docker_django/apps/smap/models.py
+++ web/docker_django/apps/smap/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=16)
+
+    @staticmethod
+    def get_or_create(name):
+        tag = Tag.objects.filter(name=name)
+        if tag:
+            return tag.first()
+        else:
+            tag = Tag(name=name)
+            tag.save()
+            return tag
+
+
+class Sumari(models.Model):
+    tags = models.ManyToManyField(Tag)
+    name = models.CharField(max_length=32)
+    message = models.CharField(max_length=140)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
--- web/docker_django/apps/smap/tests.py
+++ web/docker_django/apps/smap/tests.py
@@ -1,3 +1,116 @@
 from django.test import TestCase
+from .models import Tag, Sumari
 
-# Create your tests here.
+class Test(TestCase):
+
+    def create_data(self, data):
+        for sumari in data:
+            message = sumari["message"]
+            name = sumari["name"]
+            lat = sumari["lat"]
+            lng = sumari["lng"]
+            new_sumari = Sumari(name=name, message=message, lat=lat, lng=lng)
+            new_sumari.save()
+            for tagname in sumari["tags"]:
+                tag = Tag.get_or_create(tagname)
+                new_sumari.tags.add(tag)
+                new_sumari.save()
+
+
+    def test_search_sumaris_by_tag(self):
+        data = [
+            {
+                "tags": {
+                    "meshi",
+                    "ramen"
+                },
+                "name": "山岡屋",
+                "message": "山岡屋うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+            },
+            {
+                "tags": {
+                    "meshi",
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "すしうまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+            },
+            {
+                "tags": {
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "回転寿司うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+
+            },
+            {
+                "tags": {
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+
+            }
+        ]
+        self.create_data(data)
+
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["meshi"])), 2)
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["ramen"])), 1)
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["sushi"])), 3)
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["ramen", "sushi"])), 4)
+
+    def test_search_with_tags_not_exist(self):
+        data = [
+            {
+                "tags": {
+                    "meshi",
+                    "ramen"
+                },
+                "name": "山岡屋",
+                "message": "山岡屋うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+            },
+            {
+                "tags": {
+                    "meshi",
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "すしうまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+            },
+            {
+                "tags": {
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "回転寿司うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+
+            },
+            {
+                "tags": {
+                    "sushi"
+                },
+                "name": "すしろー",
+                "message": "うまい",
+                "lat": 41.773809,
+                "lng": 140.726467,
+
+            }
+        ]
+        self.create_data(data)
+
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["USA"])), 0)
+        self.assertEqual(len(Sumari.objects.filter(tags__name__in=["---", "0000"])), 0)

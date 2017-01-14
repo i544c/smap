6db850b663305c24dd91ef1360ddaf7e61bf5c3b
Isaac <r.sato7690@me.com>
2017-01-14 18:39:26 +0900

    Add json

--- html/script.js
+++ html/script.js
@@ -1,6 +1,8 @@
 var mapArea = document.getElementById("map");
 var isGeo = true; //端末が現在地取得に対応しているか
+var map;
 var lat = 35.709984, lng = 139.810703;
+var markers = [];
 if(!navigator.geolocation) {
   alert("あなたの端末では、現在地の取得が出来ません。");
   isGeo = false;
@@ -32,6 +34,36 @@ function getLocation() {
 }
 
 /**
+* データ取得
+*/
+function getData() {
+  return $.ajax({
+    url: "test.json",
+    type: "get"
+  });
+}
+
+/**
+* マーカーを取得する
+*/
+function getMarker() {
+  getData().then(function(data) {
+    for(var i = 0; i < data.length; i++) {
+      var lat = data[i]["position"]["lat"];
+      var lng = data[i]["position"]["lng"];
+      console.log(lat, lng);
+      markers[i] = new google.maps.Marker({
+        map: map,
+        position: {
+          lat: lat,
+          lng: lng
+        }
+      });
+    }
+  });
+};
+
+/**
 * 現在地を反映する
 */
 function initMap() {
@@ -39,13 +71,7 @@ function initMap() {
     zoom: 15,
     center: new google.maps.LatLng(lat, lng)
   };
-  var map = new google.maps.Map(mapArea, opts);
+  map = new google.maps.Map(mapArea, opts);
 
-  var marker = new google.maps.Marker({
-    map: map,
-    position: {
-      lat: 41.773809,
-      lng: 140.726467
-    }
-  });
+  getMarker();
 }
